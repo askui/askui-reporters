7a53ee13668d930fa141a47249b194e094370dc4
Adrian Stritzinger <Adrian.Stritzinger@askui.com>
2023-09-10 13:26:39 +0200

    feat: remove private utils from public package api

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,2 @@
-export { convertPngDataUrlToBuffer } from "./utils/image-reporting-utils";
-export { convertBase64StringToReadStream, writeWebmToMp4 } from "./utils/video-reporting-utils";
 export { AskUIJestHtmlStepReporter } from "./jest-html/askui-jest-html-step-reporter";
 export { AskUIAllureStepReporter } from "./allure/askui-allure-step-reporter";

b3d7c4c5f3c3de9216536fe4887df32bf5907534
Adrian Stritzinger <Adrian.Stritzinger@askui.com>
2024-02-08 18:15:04 +0100

    feat: add option to AskuiAnnotationStepReporter to write screenshots

--- src/annotation/askui-annotation-step-reporter.ts
+++ src/annotation/askui-annotation-step-reporter.ts
@@ -4,46 +4,48 @@ import path from "path";
 import { JSDOM } from "jsdom";
 import { InvalidStepStatusError } from "../error/invalid-step-status-error";
 import { MissingStepContentsError } from "../error/missing-step-contents-error";
+import { convertPngDataUrlToBuffer } from "../utils/image-reporting-utils";
 
 export enum AnnotationLevel {
   ON_FAILURE = 'onFailure',
   ALL = 'all',
 }
 
+async function writeFile(folderPath: string, fileName: string, content: string | Buffer): Promise<void> {
+  const filePath = path.join(folderPath, fileName);
+  await fs.promises.mkdir(folderPath, { recursive: true });
+  await fs.promises.writeFile(filePath, content);
+}
+
+function generateFileName(suffix: string, fileExtension: string = ""): string {
+  const currentDateTime = new Date();
+  const currentTimeStringOnlyNumbers = currentDateTime.toISOString().replace(/\D/g, '');
+  return `${currentTimeStringOnlyNumbers}_${suffix}${fileExtension}`;
+}
+
 export class AskUIAnnotationStepReporter implements Reporter {
-
-  annotationLevel? = AnnotationLevel.ON_FAILURE;
-  folderPath? = 'report';
-  fileNameSuffix? = '_testStep_annotation';
-
   config: ReporterConfig = {
     withScreenshots: 'onFailure',
     withDetectedElements: 'onFailure',
   }
 
-  constructor(annotationLevel?: AnnotationLevel, folderPath?: string, fileNameSuffix?: string) {
-    if (annotationLevel !== undefined) {
-      this.annotationLevel = annotationLevel;
-
-      if (this.annotationLevel === AnnotationLevel.ALL) {
-        this.config.withScreenshots = 'always';
-        this.config.withDetectedElements = 'always';
-      }
-    }
-    if (folderPath !== undefined) {
-      this.folderPath = folderPath;
-    }
-    if (fileNameSuffix !== undefined) {
-      this.fileNameSuffix = fileNameSuffix;
+  constructor(
+    public annotationLevel = AnnotationLevel.ON_FAILURE,
+    public folderPath = 'report',
+    public fileNameSuffix = '_testStep_annotation',
+    public withScreenshot = false,
+  ) {
+    if (this.annotationLevel === AnnotationLevel.ALL) {
+      this.config.withScreenshots = 'always';
+      this.config.withDetectedElements = 'always';
     }
   }
 
   async onStepEnd(step: Step): Promise<void> {
-
     if (step.status === 'running' || step.status === 'pending') {
       throw new InvalidStepStatusError(
-        `Cannot report on a step with status '{step.status}'. 
-        This indicates an error in the step execution.`);
+        `Cannot report on a step with status '${step.status}'. This indicates an error in the step execution.`,
+      );
     }
 
     if (this.annotationLevel === AnnotationLevel.ON_FAILURE && step.status === 'passed') {
@@ -51,9 +53,9 @@ export class AskUIAnnotationStepReporter implements Reporter {
     }
 
     if (step.lastRun?.end?.screenshot === undefined ||
-        step.lastRun?.end?.detectedElements === undefined) {
-      throw new MissingStepContentsError("'screenshot' or 'detectedElements' not defined");
-    }
+      step.lastRun?.end?.detectedElements === undefined) {
+    throw new MissingStepContentsError("'screenshot' or 'detectedElements' not defined");
+  }
 
     const annotation = new Annotation(
       step.lastRun.end.screenshot,
@@ -63,21 +65,15 @@ export class AskUIAnnotationStepReporter implements Reporter {
     const stepStatus = step.status=='passed' ? 'passed' : 'failed';
 
     const suffix = `${stepStatus}${this.fileNameSuffix}`
-    await AskUIAnnotationStepReporter.writeAnnotation(
-      annotation.toHtml(),
-      this.folderPath,
-      suffix);
+    const fileName = generateFileName(suffix);
+    await writeFile(this.folderPath, `${fileName}.html`, annotation.toHtml().serialize());
+    await writeFile(this.folderPath, `${fileName}_screenshot.png`, convertPngDataUrlToBuffer(step.lastRun.end.screenshot));
   }
 
+  /**
+   * @deprecated The method should not be used or overriden.
+   */
   static async writeAnnotation(html: JSDOM, outputFolder = 'report', fileNameSuffix = 'annotation') {
-    const currentDateTime = new Date();
-    const currentTimeStringOnlyNumbers = currentDateTime.toISOString().replace(/\D/g, '');
-    const fileName = `${currentTimeStringOnlyNumbers}_${fileNameSuffix}.html`;
-    const outputFilePath = path.join(outputFolder, fileName);
-    if (!(fs.existsSync(outputFolder))) {
-      fs.mkdirSync(outputFolder, { recursive: true });
-    }
-    fs.writeFileSync(outputFilePath, html.serialize());
+    await writeFile(outputFolder, generateFileName(fileNameSuffix), html.serialize());
   }
-
 }
